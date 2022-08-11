d8822897b57e7f10011f4c920c48f770ab949663
DavidSaavedraVazquez <davidsv20@icloud.com>
2022-08-11 13:37:23 -0500

    Add boostrap-react

--- client/src/containers/login.jsx
+++ client/src/containers/login.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/login.scss';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
+    const form = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+            const formData = new FormData(form.current);
+            const data = {
+                username: formData.get('email'),
+                password: formData.get('password')
+            }
+            console.log(data);
+    }
     return (    
-        <form action="/login" method="POST">
-            <h1>Welcome to Data Warehouse</h1>
-            <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            </div>
-            <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1"/>
-            </div>
-            <button id="submitBtn" type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <Form action='/login' method="POST" ref={form} className='form'>
+            <h1>Bienvenido a Warehouse</h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" className='input' type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control name="password" className='input' type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        </Form.Group>
+        <Button onClick={handleSubmit} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>  
     );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
 import './styles/master.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 ReactDOM.render(<App/>, document.getElementById('app'));
