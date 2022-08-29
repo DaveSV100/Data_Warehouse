f1fe96509b71658c0fe331150cc038bbdaaf2cbd
DavidSaavedraVazquez <davidsv20@icloud.com>
2022-08-28 23:13:00 -0500

    Fix bugs

--- frontend/src/pages/region.js
+++ frontend/src/pages/region.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styles from '@styles/Region.module.scss'
+
 const region = () => {
+    const [countries2, setCountries2] = useState(false);
+    const [display, setDisplay] = useState(false);
+    
+    const toggleDisplay = () => {
+        setDisplay(!display);
+      };
+
     return (
-        <h1>Región/Ciudad</h1>
+        <>    
+            <h1>Región/Ciudad</h1>
+            <div>
+                    <ul>
+                        <li>Norteamérica
+                            <ul>
+                                <li>Estados Unidos</li>
+                                <li>México</li>
+                            </ul>
+                        </li>
+                        <li onClick={() => setCountries2(!countries2)} className={styles.region}>Sudamérica
+                            {
+                                countries2 && 
+                                <ul>
+                                    <li onClick={() => toggleDisplay()}>Argentina
+                                        
+                                            <ul className={display ? 'styles.active' : 'styles.unactive'}>
+                                                <li>Buenos Aires</li>
+                                                <li>Rosario</li>
+                                            </ul>
+            
+                                    </li>
+                                    <li>Colombia</li>
+                                </ul>
+                            }
+                        </li>
+                        <li>Europa
+                            <ul>
+                                <li>España</li>
+                                <li>Francia</li>
+                            </ul>
+                        </li>
+                    </ul>
+
+            </div>
+        </>
     );
 }
 
